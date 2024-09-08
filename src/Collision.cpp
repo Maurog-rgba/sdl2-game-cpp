a9c0fea2ea2a0cd8d860efed89d13cf4202d3ced
Mauro Gabriel dos Santos <gabriel.mauro.santos.3@gmail.com>
2024-09-08 14:41:42 -0300

    feat: Add AABB collision detection for ColliderComponents

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -1,4 +1,5 @@
 #include "Collision.hpp"
+#include "ECS/ColliderComponent.hpp"
 
 bool Collision::AABB(const SDL_Rect &recA, const SDL_Rect &recB)
 {
@@ -7,6 +8,16 @@ bool Collision::AABB(const SDL_Rect &recA, const SDL_Rect &recB)
         recA.y + recA.h >= recB.y &&
         recB.y + recB.h >= recA.y)
     {
+        return true;
+    }
+    return false;
+}
+
+bool Collision::AABB(const ColliderComponent &colA, const ColliderComponent &colB)
+{
+    if (AABB(colA.collider, colB.collider))
+    {
+        std::cout << colA.tag << " hit: " << colB.tag << std::endl;
         return true;
     }
     return false;
